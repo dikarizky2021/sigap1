a4b048828d03c14e44bde7c373085fc1ebc3aaed
dikarizky2021 <dikarizky2021@polinema.ac.id>
2022-06-26 22:24:07 +0700

    test

---
+++ app/src/main/java/com/example/sigap1/URLs.java
@@ -0,0 +1,8 @@
+package com.example.sigap1;
+
+public class URLs {
+    private static final String ROOT_URL = "http://tahutekno.com/tesdika/Api.php?apicall=";
+
+
+    public static final String URL_LOGIN= ROOT_URL + "login";
+}
---
+++ app/src/main/java/com/example/sigap1/User.java
@@ -0,0 +1,24 @@
+package com.example.sigap1;
+
+public class User {
+
+
+    private String username, nama;
+
+    public User( String username, String nama) {
+
+        this.username = username;
+        this.nama= nama;
+
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+}
